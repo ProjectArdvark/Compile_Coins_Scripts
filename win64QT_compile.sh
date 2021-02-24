691b09c0236286e1bf533f3f01d2f91d2c922ac6
Spectre Security <42466179+SpectreSecurity@users.noreply.github.com>
2021-02-24 15:33:32 -0500

    Update win64QT_compile.sh

--- win64QT_compile.sh
+++ win64QT_compile.sh
@@ -15,6 +15,6 @@ sudo ../dist/configure --enable-cxx --disable-shared --with-pic --prefix=`pwd`/d
 sudo make install
 cd ../../
 sudo ./autogen.sh
-sudo ./configure LDFLAGS="-L`pwd`/db4/lib/" CPPFLAGS="-I`pwd`/db4/include/" --prefix=`pwd`/depends/x86_64-w64-mingw32
+sudo ./configure --disable-online-rust LDFLAGS="-L`pwd`/db4/lib/" CPPFLAGS="-I`pwd`/db4/include/" --prefix=`pwd`/depends/x86_64-w64-mingw32
 sudo make -j2
 echo "Remember to strip the QT file!"
