3f5d85ab387905c7fc1c5700050b99e6ea0c88e3
Spectre Security <42466179+SpectreSecurity@users.noreply.github.com>
2021-04-16 23:22:58 -0400

    Update linux64_compile.sh

--- linux64_compile.sh
+++ linux64_compile.sh
@@ -14,6 +14,6 @@ sudo ../dist/configure --enable-cxx --disable-shared --with-pic --prefix=`pwd`/d
 sudo make install
 cd ../../
 sudo ./autogen.sh
-sudo ./configure LDFLAGS="-L`pwd`/db4/lib/" CPPFLAGS="-I`pwd`/db4/include/" --disable-online-rust --prefix=`pwd`/depends/x86_64-pc-linux-gnu
+sudo ./configure --disable-online-rust LDFLAGS="-L`pwd`/db4/lib/" CPPFLAGS="-I`pwd`/db4/include/" --prefix=`pwd`/depends/x86_64-pc-linux-gnu
 sudo make -j2
 echo "Remember to strip the daemon, cli, and tx files!"
