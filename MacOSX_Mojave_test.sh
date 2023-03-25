a730349752b1997db16bac6a0245b33be7f39130
Spectre Security <42466179+SpectreSecurity@users.noreply.github.com>
2023-03-25 00:52:41 -0400

    Update MacOSX_Mojave_test.sh

--- MacOSX_Mojave_test.sh
+++ MacOSX_Mojave_test.sh
@@ -2,6 +2,7 @@
 sudo make clean
 sudo apt-get update
 sudo apt-get -y upgrade
+sudo chmod -Rv 775
 cd `pwd`/depends
 sudo mkdir SDKs
 cd SDKs
@@ -20,6 +21,7 @@ cd `pwd`/db-4.8.30.NC/build_unix/
 sudo ../dist/configure --enable-cxx --disable-shared --with-pic --prefix=`pwd`/db4
 sudo make install
 cd ../../
+sudo chmod -Rv 775
 sudo ./autogen.sh
 sudo ./configure --disable-online-rust --prefix=`pwd`/depends/x86_64-apple-darwin14
 sudo make -j2
